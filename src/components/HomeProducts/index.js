941ebe3f72c5987f39a960e14ffc078039dad31c
GawraHari <bhonagirinaveen1@gmail.com>
2025-11-13 11:21:57 +0000

    mobile-view added

--- src/components/HomeProducts/index.js
+++ src/components/HomeProducts/index.js
@@ -8,24 +8,34 @@ const HomeProduct = props => {
   const {allProducts} = props
 
   return (
-    <div className="dataBg">
-      <ul className="dataBg-ul">
-        {allProducts.categories.map(eachCat => (
-          <li key={eachCat.name} id={eachCat.name} className="dataBg-li">
-            <div className="catIcon">
-              <p className="catname">{eachCat.name}</p>
-              <IoIosArrowForward size={23} color="black" />
-            </div>
-            <ul className="eachCatItems">
-              {eachCat.products.map(eachItem => (
-                <ProductItem key={eachItem.id} details={eachItem} />
-              ))}
-            </ul>
-            <hr className="horizon" />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <nav className="mbViewHeader">
+        <img
+          className="website-logo-sm"
+          src="https://res.cloudinary.com/dndlbcblt/image/upload/v1761044119/Logo_1_g4peag.png"
+          alt="website logo"
+        />
+        <p>Home</p>
+      </nav>
+      <div className="dataBg">
+        <ul className="dataBg-ul">
+          {allProducts.categories.map(eachCat => (
+            <li key={eachCat.name} id={eachCat.name} className="dataBg-li">
+              <div className="catIcon">
+                <p className="catname">{eachCat.name}</p>
+                <IoIosArrowForward size={23} color="black" />
+              </div>
+              <ul className="eachCatItems">
+                {eachCat.products.map(eachItem => (
+                  <ProductItem key={eachItem.id} details={eachItem} />
+                ))}
+              </ul>
+              <hr className="horizon" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 export default HomeProduct
